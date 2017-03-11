f8032c97c811675a89861391b250b97c004d32c9
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-10 20:08:58 -0800

    hien thi thong tin chuyen muc & bai viet cho trang blog

--- app/controllers/erp/online_store/frontend/blog_controller.rb
+++ app/controllers/erp/online_store/frontend/blog_controller.rb
@@ -1,13 +1,17 @@
 module Erp
   module OnlineStore
     module Frontend
-      class BlogController < Erp::Frontend::FrontendController
+      class BlogController < Erp::Frontend::FrontendController          
         def index
           @body_class = "res layout-subpage banners-effect-6"
+          @blogs = Erp::Articles::Article.search(params).paginate(:page => params[:page], :per_page => 1)
+          @categories = Erp::Articles::Category.all
         end
     
         def detail
           @body_class = "res layout-subpage"
+          @blog = Erp::Articles::Article.find(params[:blog_id])
+          @categories = Erp::Articles::Category.all
         end
       end
     end
