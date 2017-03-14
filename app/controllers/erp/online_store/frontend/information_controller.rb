6456667b79b65105efcbcb94b4f8308093ece1b0
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-13 20:21:11 -0700

    chuc nang gui tin lien he

--- app/controllers/erp/online_store/frontend/information_controller.rb
+++ app/controllers/erp/online_store/frontend/information_controller.rb
@@ -12,6 +12,22 @@ module Erp
     
         def contact_us
           @body_class = "res layout-subpage"
+          
+          if params[:contact].present?
+            @contact = Erp::Contacts::Contact.new(contact_params)
+            @contact.contact_type = Erp::Contacts::Contact::TYPE_PERSON
+            if @contact.save and params[:msg].present?
+              @msg = Erp::Contacts::Message.new(message_params)
+              @msg.contact_id = @contact.id
+              @msg.to_contact_id = Erp::Contacts::Contact.first.id
+              respond_to do |format|
+                if @msg.save
+                  Erp::Contacts::ContactMailer.sending_email_contact(@msg).deliver_now
+                  format.html { redirect_to :back, notice: 'Yêu cầu đã gửi thành công. Chúng tôi sẽ liên hệ cho Quý khách trong thời gian sớm nhất.' }
+                end
+              end
+            end
+          end
         end
     
         def terms_and_conditions
@@ -20,6 +36,15 @@ module Erp
         def site_map
           @body_class = "res layout-subpage"
         end
+        
+        private
+          def contact_params
+            params.fetch(:contact, {}).permit(:name, :email, :phone)
+          end
+          
+          def message_params
+            params.fetch(:msg, {}).permit(:message)
+          end
       end
     end
   end
