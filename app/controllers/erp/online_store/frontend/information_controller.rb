c1e995d5712ed00b7b36bd05b469a74eea9abb1e
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-14 20:13:44 -0700

    update flash notice/error for contact_us

--- app/controllers/erp/online_store/frontend/information_controller.rb
+++ app/controllers/erp/online_store/frontend/information_controller.rb
@@ -23,9 +23,11 @@ module Erp
               respond_to do |format|
                 if @msg.save
                   Erp::Contacts::ContactMailer.sending_email_contact(@msg).deliver_now
-                  format.html { redirect_to :back, notice: 'Yêu cầu đã gửi thành công. Chúng tôi sẽ liên hệ cho Quý khách trong thời gian sớm nhất.' }
+                  format.html { redirect_to :back, notice: 'Yêu cầu đã gửi thành công.\n Chúng tôi sẽ liên hệ cho bạn trong thời gian sớm nhất.' }
                 end
               end
+            else
+              redirect_to :back, flash: { error: 'Không thể gửi đi do một số trường đang bị bỏ trống.' }
             end
           end
         end
