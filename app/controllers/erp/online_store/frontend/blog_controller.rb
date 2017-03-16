5eff67cf9f39022e58a7eaaaf64839aa8c26bd42
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-15 23:28:03 -0700

    get blogs with alias = ALIAS_BLOG

--- app/controllers/erp/online_store/frontend/blog_controller.rb
+++ app/controllers/erp/online_store/frontend/blog_controller.rb
@@ -4,14 +4,14 @@ module Erp
       class BlogController < Erp::Frontend::FrontendController          
         def index
           @body_class = "res layout-subpage banners-effect-6"
-          @blogs = Erp::Articles::Article.search(params).paginate(:page => params[:page], :per_page => 1)
-          @categories = Erp::Articles::Category.all
+          @blogs = Erp::Articles::Article.search(params).paginate(:page => params[:page], :per_page => 2)
+          @categories = Erp::Articles::Category.where(alias: Erp::Articles::Category::ALIAS_BLOG)
         end
     
         def detail
           @body_class = "res layout-subpage"
           @blog = Erp::Articles::Article.find(params[:blog_id])
-          @categories = Erp::Articles::Category.all
+          @categories = Erp::Articles::Category.where(alias: Erp::Articles::Category::ALIAS_BLOG)
         end
       end
     end
