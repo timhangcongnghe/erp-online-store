95a8ce5e0c3d15d8fe7e6739624f0957de592c27
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-16 02:27:10 -0700

    fix index action: change search to get_all_blogs

--- app/controllers/erp/online_store/frontend/blog_controller.rb
+++ app/controllers/erp/online_store/frontend/blog_controller.rb
@@ -4,7 +4,7 @@ module Erp
       class BlogController < Erp::Frontend::FrontendController          
         def index
           @body_class = "res layout-subpage banners-effect-6"
-          @blogs = Erp::Articles::Article.search(params).paginate(:page => params[:page], :per_page => 2)
+          @blogs = Erp::Articles::Article.get_all_blogs(params).paginate(:page => params[:page], :per_page => 5)
           @categories = Erp::Articles::Category.where(alias: Erp::Articles::Category::ALIAS_BLOG)
         end
     
