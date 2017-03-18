91cad2fbccaf67cdb7a8a1b2ac07eaba2789c784
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-17 21:09:04 -0700

    add comment to blog controller + get comments list

--- app/controllers/erp/online_store/frontend/blog_controller.rb
+++ app/controllers/erp/online_store/frontend/blog_controller.rb
@@ -12,7 +12,22 @@ module Erp
           @body_class = "res layout-subpage"
           @blog = Erp::Articles::Article.find(params[:blog_id])
           @categories = Erp::Articles::Category.where(alias: Erp::Articles::Category::ALIAS_BLOG)
+          @comments = Erp::Articles::Comment.where(article_id: params[:blog_id]).order('created_at DESC').paginate(:page => params[:page], :per_page => 1)
+          
+          if params[:comment].present?
+            @comment = Erp::Articles::Comment.new(comment_params)
+            if @comment.save
+              redirect_to :back, flash: { success: 'Bình luận thành công.' }
+            else
+              redirect_to :back, flash: { error: 'Không bình luận được. Vui lòng kiểm tra thông tin đã nhập.' }
+            end
+          end
         end
+        
+        private
+          def comment_params
+            params.fetch(:comment, {}).permit(:name, :email, :message, :article_id)
+          end
       end
     end
   end
