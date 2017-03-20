77fd82cf3365ab62f00859e675b77c8ff3760272
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2017-03-19 19:47:43 -0700

    get product images for product page

--- app/controllers/erp/online_store/frontend/category_controller.rb
+++ app/controllers/erp/online_store/frontend/category_controller.rb
@@ -5,6 +5,7 @@ module Erp
         def index
           @body_class = "res layout-subpage"
           @menu = Erp::Menus::Menu.find(params[:menu_id])
+          @related_menus = @menu.related_menus.limit(5)
           @products = @menu.get_products_for_categories(params)
           @meta_keywords = @menu.meta_keywords
           @meta_desciption = @menu.meta_description
