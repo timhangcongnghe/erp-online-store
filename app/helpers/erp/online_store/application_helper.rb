49e2827eee5c0b813838ce704977ee4f88c21435
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-26 21:10:35 -0700

    update display stars helper

--- app/helpers/erp/online_store/application_helper.rb
+++ app/helpers/erp/online_store/application_helper.rb
@@ -1,6 +1,16 @@
 module Erp
   module OnlineStore
     module ApplicationHelper
+      
+      # display stars rating
+      def stars(number)
+        strs = []
+        (1..5).each do |star|
+          strs << "<span class=\"fa fa-stack\"><i class=\"fa fa-star#{(number >= star ? '' : '-o')} fa-stack-2x\"></i></span>"
+        end
+        strs.join('')
+      end
+      
     end
   end
 end
