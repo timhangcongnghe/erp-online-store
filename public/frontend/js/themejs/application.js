ccf0b2660fbbbad78a45ab7e89b176cf9fd9c2d7
Luan Pham <luanpm88@gmail.com>
2017-03-31 01:28:44 -0700

    fix slider js

--- public/frontend/js/themejs/application.js
+++ public/frontend/js/themejs/application.js
@@ -2430,31 +2430,31 @@ $(document).ready(function() {
 });
 $(document).ready(function($) {
     "use strict";
-    //$('.list-msg').owlCarousel2({
-    //    pagination: false,
-    //    center: false,
-    //    nav: false,
-    //    loop: true,
-    //    slideBy: 1,
-    //    autoplay: true,
-    //    margin: 30,
-    //    autoplayTimeout: 4500,
-    //    autoplayHoverPause: true,
-    //    autoplaySpeed: 1200,
-    //    startPosition: 0,
-    //    responsive: {
-    //        0: {
-    //            items: 1
-    //        },
-    //        480: {
-    //            items: 1
-    //        },
-    //        768: {
-    //            items: 1
-    //        },
-    //        1200: {
-    //            items: 1
-    //        }
-    //    }
-    //});
+    $('.list-msg').owlCarousel2({
+        pagination: false,
+        center: false,
+        nav: false,
+        loop: true,
+        slideBy: 1,
+        autoplay: true,
+        margin: 30,
+        autoplayTimeout: 4500,
+        autoplayHoverPause: true,
+        autoplaySpeed: 1200,
+        startPosition: 0,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            1200: {
+                items: 1
+            }
+        }
+    });
 });
