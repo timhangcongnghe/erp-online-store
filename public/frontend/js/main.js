f4e49f631b18993a3a1858315ed16057b711d7be
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-31 20:09:40 -0700

    layout upload image for my account

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -391,8 +391,69 @@ $(document).ready(function () {
             }
         });
     });
-});
-$(window).scroll(function () {
+    
+    
+    // TODO: JS CHO UPLOAD HÌNH TRÊN MY ACCOUNT
+    $(document).on('click', '#close-preview', function(){ 
+        $('.image-preview').popover('hide');
+        // Hover befor close the preview
+        $('.image-preview').hover(
+            function () {
+               $('.image-preview').popover('show');
+            }, 
+             function () {
+               $('.image-preview').popover('hide');
+            }
+        );    
+    });
+    
+    $(function() {
+        // Create the close button
+        var closebtn = $('<button/>', {
+            type:"button",
+            text: 'x',
+            id: 'close-preview',
+            style: 'font-size: initial;',
+        });
+        closebtn.attr("class","close pull-right");
+        // Set the popover default content
+        $('.image-preview').popover({
+            trigger:'manual',
+            html:true,
+            title: "<strong>Xem trước</strong>"+$(closebtn)[0].outerHTML,
+            content: "There's no image",
+            placement:'bottom'
+        });
+        // Clear event
+        $('.image-preview-clear').click(function(){
+            $('.image-preview').attr("data-content","").popover('hide');
+            $('.image-preview-filename').val("");
+            $('.image-preview-clear').hide();
+            $('.image-preview-input input:file').val("");
+            $(".image-preview-input-title").text("Chọn ảnh"); 
+        }); 
+        // Create the preview image
+        $(".image-preview-input input:file").change(function (){     
+            var img = $('<img/>', {
+                id: 'dynamic',
+                width:250,
+                height:200
+            });      
+            var file = this.files[0];
+            var reader = new FileReader();
+            // Set preview image into the popover data-content
+            reader.onload = function (e) {
+                $(".image-preview-input-title").text("Thay đổi");
+                $(".image-preview-clear").show();
+                $(".image-preview-filename").val(file.name);            
+                img.attr('src', e.target.result);
+                $(".image-preview").attr("data-content",$(img)[0].outerHTML).popover("show");
+            }
+            reader.readAsDataURL(file);
+        });  
+    });
+    // TODO: JS CHO UPLOAD HÌNH TRÊN MY ACCOUNT ----- END
+});$(window).scroll(function () {
     var top = $(window).scrollTop();
 
     if (top > 165 &&  $(window).width() > 992)  {
