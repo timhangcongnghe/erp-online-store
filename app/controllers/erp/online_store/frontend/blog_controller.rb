16721f1ebaf2f3c6482d265ca0543641ec85910d
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-04-05 00:58:07 -0700

    get categories by alias blog

--- app/controllers/erp/online_store/frontend/blog_controller.rb
+++ app/controllers/erp/online_store/frontend/blog_controller.rb
@@ -7,7 +7,7 @@ module Erp
         def index
           @body_class = "res layout-subpage banners-effect-6"
           @blogs = Erp::Articles::Article.get_all_blogs(params).paginate(:page => params[:page], :per_page => 5)
-          @categories = Erp::Articles::Category.where(alias: Erp::Articles::Category::ALIAS_BLOG)
+          @categories = Erp::Articles::Category.get_categories_by_alias_blog
           if params[:cat_id].present?
             @category = Erp::Articles::Category.find(params[:cat_id])
           end
