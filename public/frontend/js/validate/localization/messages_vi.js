d0dd78a2f929adb5dc81cc73256bc7cfd6c135f4
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-04-09 21:37:08 -0700

    validate update

--- public/frontend/js/validate/localization/messages_vi.js
+++ public/frontend/js/validate/localization/messages_vi.js
@@ -13,9 +13,9 @@
  * Locale: VI (Vietnamese; Tiếng Việt)
  */
 $.extend( $.validator.messages, {
-	required: "Không được để trống trường này.",
+	required: "Yêu cầu nhập trường này.",
 	remote: "Hãy sửa cho đúng.",
-	email: "Hãy nhập email hợp lệ.",
+	email: "Vui lòng cung cấp địa chỉ email hợp lệ.",
 	url: "Hãy nhập URL.",
 	date: "Hãy nhập ngày.",
 	dateISO: "Hãy nhập ngày (ISO).",
