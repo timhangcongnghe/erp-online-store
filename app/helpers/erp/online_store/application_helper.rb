c6562ba16377fbc7fb055690529bf6b11da3484e
Luan Pham <luanpm88@gmail.com>
2017-04-27 02:55:45 -0700

    update product details breadscrump

--- app/helpers/erp/online_store/application_helper.rb
+++ app/helpers/erp/online_store/application_helper.rb
@@ -27,7 +27,7 @@ module Erp
 
       # product link helper
       def product_link(product)
-        erp_online_store.product_detail_path(product_id: product.id, title: url_friendly(product.product_name))
+        erp_online_store.product_detail_path(product_id: product.id, title: url_friendly(product.product_name), menu_id: params[:menu_id])
       end
 
       # brand link helper
