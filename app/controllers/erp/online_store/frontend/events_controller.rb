42516cb370a142587f76f1c7575642bdbad4a60a
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-04-28 01:08:17 -0700

    get event products

--- app/controllers/erp/online_store/frontend/events_controller.rb
+++ app/controllers/erp/online_store/frontend/events_controller.rb
@@ -4,8 +4,8 @@ module Erp
       class EventsController < Erp::Frontend::FrontendController
         def index
         end
-        def events_detail
-          @products = Erp::Products::Product.get_deal_products.paginate(:page => params[:page], :per_page => 16)
+        def event_detail
+          @products = Erp::Products::Event.get_event_products(params).paginate(:page => params[:page], :per_page => 16)
           @menus = Erp::Menus::Menu.get_menus
         end
       end
