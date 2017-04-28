3aef61e1d91852a6b66e7a05d8603cb23b3bd045
Luan Pham <luanpm88@gmail.com>
2017-04-27 20:29:27 -0700

    permission comment/rating

--- app/decorators/models/ability_decorator.rb
+++ app/decorators/models/ability_decorator.rb
@@ -1,13 +1,25 @@
 Erp::Ability.class_eval do
   def online_store_ability(user)
-    can :delete, Erp::Products::Comment do |comment|
-      !user.nil? && user.backend_access
+    can :create, Erp::Products::Comment do |comment|
+      !user.nil? && user.get_permissions["products_comments"]["create"] == "yes"
     end
-    can :delete, Erp::Articles::Comment do |comment|
-      !user.nil? && user.backend_access
+    can :delete, Erp::Products::Comment do |comment|
+      !user.nil? && user.backend_access && user.get_permissions["products_comments"]["delete"] == "yes"
+    end
+
+
+    can :create, Erp::Products::Rating do |rating|
+      !user.nil? && user.get_permissions["products_ratings"]["create"] == "yes"
     end
     can :delete, Erp::Products::Rating do  |rating|
-      !user.nil? && (user.backend_access or rating.user_id == user.id)
+      !user.nil? && (user.backend_access or rating.user_id == user.id) && user.get_permissions["products_ratings"]["delete"] == "yes"
+    end
+
+    can :create, Erp::Articles::Comment do |comment|
+      !user.nil? && user.get_permissions["articles_comments"]["create"] == "yes"
+    end
+    can :delete, Erp::Articles::Comment do |comment|
+      !user.nil? && user.backend_access && user.get_permissions["articles_comments"]["delete"] == "yes"
     end
   end
 end
