08c4088e2720170c569e45566f7af69d56a98126
Luan Pham <luanpm88@gmail.com>
2017-05-19 00:29:39 -0700

    update business sidebar

--- app/controllers/erp/online_store/frontend/business_choices_controller.rb
+++ app/controllers/erp/online_store/frontend/business_choices_controller.rb
@@ -4,7 +4,7 @@ module Erp
       class BusinessChoicesController < Erp::Frontend::FrontendController
         def index
           @body_class = "common-home res layout-home7 loaded"
-          @products = Erp::Products::Product.get_business_choices.paginate(:page => params[:page], :per_page => 8)
+          @products = Erp::Products::Product.get_business_choices.frontend_filter(params).paginate(:page => params[:page], :per_page => 8)
           @menus = Erp::Menus::Menu.get_menus
         end
       end
