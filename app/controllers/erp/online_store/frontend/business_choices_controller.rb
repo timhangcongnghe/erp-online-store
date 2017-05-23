4435cf08b41a734b71cc24ea1eaf1ecede59f46b
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-05-22 21:19:35 -0700

    update @sliders to index action for business choices controller

--- app/controllers/erp/online_store/frontend/business_choices_controller.rb
+++ app/controllers/erp/online_store/frontend/business_choices_controller.rb
@@ -6,6 +6,7 @@ module Erp
           @body_class = "common-home res layout-home7 loaded"
           @products = Erp::Products::Product.get_business_choices.frontend_filter(params).paginate(:page => params[:page], :per_page => 8)
           @menus = Erp::Menus::Menu.get_menus
+          @sliders = Erp::Banners::Banner.get_home_sliders
         end
       end
     end
