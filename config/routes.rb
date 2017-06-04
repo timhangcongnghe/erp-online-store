fb8138ba5edf5452e5c4d7732785327c54e1d1bc
Son Nguyen <sonnn@hoangkhang.com.vn>
2017-06-04 10:18:15 +0700

    fix router comment blog and show login if user not admin

--- config/routes.rb
+++ config/routes.rb
@@ -30,8 +30,8 @@ Erp::OnlineStore::Engine.routes.draw do
   get "tin-cong-nghe.html" => "frontend/blog#index", as: :blog
   get "tin-cong-nghe/:blog_id(/:title).html" => "frontend/blog#detail", as: :blog_detail
   get "tin-cong-nghe/chu-de/:cat_id(/:title).html" => "frontend/blog#index", as: :blog_with_category
-  get "binh-luan-bai-viet.html/:blog_id" => "frontend/blog#comments", as: :blog_comments
-  post "binh-luan-bai-viet.html/:blog_id" => "frontend/blog#comments"
+  get "binh-luan-bai-viet/:blog_id.html" => "frontend/blog#comments", as: :blog_comments
+  post "binh-luan-bai-viet/:blog_id.html" => "frontend/blog#comments"
   delete "binh-luan-bai-viet/:comment_id-remove.html" => "frontend/blog#delete_comment", as: :delete_blog_comment
 
   # EVENTS
