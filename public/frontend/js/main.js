8222f446d25519e94b8d631798b17ab165aeb997
Linh.nguyen.31 <ntrilinh@GMAIL.com>
2017-06-09 01:37:25 -0700

    custom buttom left right for zoom image detail product

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -1014,10 +1014,7 @@ $(window).scroll(function () {
     btnMenuFixed();
     showHideMenuFixed();
 });
-$( window ).resize(function() {
-    // auto height product list
-    fixWithProductListAll();
-});
+
 
 
 // Custom js
@@ -1120,3 +1117,30 @@ $('.item-vertical').hover(function() {
     }, function() {
         $(this).removeClass('active1');
 });
+
+// zoom images detail product
+$( document ).ready(function() {
+    $('.large-image').click( function() {
+        setTimeout(function(){
+            autoWidthImg();
+        }, 70);
+    });
+    
+    function autoWidthImg() {
+        if ($('.mfp-arrow-right').length>0)
+        {
+            var width = ((($(document).width() - $('.mfp-content').width()) / 2) - 15);
+            $('.mfp-arrow-left').attr("id","my-mft-left");
+            $('.mfp-arrow-right').attr("id","my-mft-right");
+            document.getElementById("my-mft-left").style.left = width + "px";
+            document.getElementById("my-mft-right").style.right = width + "px";
+        }
+    }
+    
+    $( window ).resize(function() {
+        fixWithProductListAll();
+        autoWidthImg();
+    });
+});
+
+ 
