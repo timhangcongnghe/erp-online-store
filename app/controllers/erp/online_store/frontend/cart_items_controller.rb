592a53bbad4b295fa4abbec99435cf5d7664cddc
Luan Pham <luanpm88@gmail.com>
2017-07-19 20:28:57 -0700

    fix render text

--- app/controllers/erp/online_store/frontend/cart_items_controller.rb
+++ app/controllers/erp/online_store/frontend/cart_items_controller.rb
@@ -1,23 +1,23 @@
 module Erp
   module OnlineStore
     module Frontend
-      class CartItemsController < Erp::Frontend::FrontendController        
+      class CartItemsController < Erp::Frontend::FrontendController
         # POST /cart_items
         # POST /cart_items.json
         def add_to_cart
           quantity = params[:quantity]
           product = Erp::Products::Product.find(params[:product_id])
           @cart_item = @cart.add_product(product.id, quantity)
-      
+
           if @cart_item.save
-            render text: 'Thêm sản phẩm vào giỏ hàng thành công.'
+            render plain: 'Thêm sản phẩm vào giỏ hàng thành công.'
           end
         end
-        
+
         def remove_cart_item
-          @cart.remove_cart_item(params[:cart_item_id])          
+          @cart.remove_cart_item(params[:cart_item_id])
           redirect_to erp_online_store.shopping_cart_path, notice: 'Xóa sản phẩm khỏi giỏ hàng thành công.'
-        end    
+        end
       end
     end
   end
