4818a85479449e2c23e550d9d49e3f2c207b1c43
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-07-30 01:55:31 -0700

    update event detail page

--- app/controllers/erp/online_store/frontend/events_controller.rb
+++ app/controllers/erp/online_store/frontend/events_controller.rb
@@ -3,11 +3,13 @@ module Erp
     module Frontend
       class EventsController < Erp::Frontend::FrontendController
         def index
-          @meta_description = "Các chương trình sự kiện của TimHangCongNghe.VN với những ưu đãi, giảm giá cực hấp dẩn, đa dạng mặt hàng, thoải mái lựa chọn"
+          @meta_description = "Các chương trình sự kiện của Tìm Hàng Công Nghệ với những ưu đãi, giảm giá cực hấp dẫn, đa dạng mặt hàng, thoải mái lựa chọn"
         end
         def event_detail
           @products = Erp::Products::Event.get_event_products(params).paginate(:page => params[:page], :per_page => 16)
-          @menus = Erp::Menus::Menu.get_menus          
+          @menus = Erp::Menus::Menu.get_menus
+          @event = Erp::Products::Event.find(params[:event_id])
+          @meta_description = "Tìm Hàng Công Nghệ - " + @event.name
         end
       end
     end
