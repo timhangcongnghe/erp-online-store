aa593de0c46e71fd9e07d2de5d688073dcd7bbb5
Son Nguyen <sonnn@hoangkhang.com.vn>
2017-08-08 11:11:59 +0700

    update business choice pages

--- app/controllers/erp/online_store/frontend/business_choices_controller.rb
+++ app/controllers/erp/online_store/frontend/business_choices_controller.rb
@@ -4,7 +4,7 @@ module Erp
       class BusinessChoicesController < Erp::Frontend::FrontendController
         def index
           @body_class = "common-home res layout-home7 loaded"
-          @products = Erp::Products::Product.get_business_choices.frontend_filter(params).paginate(:page => params[:page], :per_page => 8)
+          @products = Erp::Products::Product.get_business_choices.frontend_filter(params).paginate(:page => params[:page], :per_page => 20)
           @menus = Erp::Menus::Menu.get_menus
           @sliders = Erp::Banners::Banner.get_home_sliders
           @meta_description = "Một nét mới lạ tại TimHangCongNghe.VN, các sản phẩm được các doanh nghiệp lựa chọn và tin dùng đều được giới thiệu tại trang Lựa Chọn Từ Doanh Nghiệp"
