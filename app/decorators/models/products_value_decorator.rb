ccd2a71d877c9c4d49792f54bccec6907833ab96
Luan Pham <luanpm88@gmail.com>
2018-01-09 14:12:16 +0700

    product value trigger

---
+++ app/decorators/models/products_value_decorator.rb
@@ -0,0 +1,7 @@
+Erp::Products::ProductsValue.class_eval do
+  after_save :hkerp_set_cache_thcn_properties
+
+  def hkerp_set_cache_thcn_properties
+    self.product.hkerp_set_cache_thcn_properties if self.product.present?
+  end
+end
