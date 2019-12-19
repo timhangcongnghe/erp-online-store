57f4b31cd81df77c93b81faa85c9cb8cb8e5f01e
Nguyen Ngoc Son <sonnn@hoangkhang.com.vn>
2019-12-19 02:18:37 +0000

    test 03

--- app/helpers/erp/online_store/application_helper.rb
+++ app/helpers/erp/online_store/application_helper.rb
@@ -130,8 +130,8 @@ module Erp
 
       def quick_actions(product)
         "<div class=\'view-buy hidden-xs\'>
-            <a href=\'#{erp_online_store.product_quickview_path(product_id: product, title: url_friendly(product.product_name))}\' class=\'btn-cus quick-view btn_view ajax-link\' data-type=\'ajax\'><span>Xem nhanh</span></a>
-            <a href=\'#\' class=\'btn-cus quick-view btn_view btn_compare\' product_id=\'#{product.id}\' data-url=\'#{erp_online_store.add_to_compare_frontend_compare_items_path}\' title=\'So sánh sản phẩm\'><span>So sánh</span></a>
+            <a href=\'#{erp_online_store.product_quickview_path(product_id: product, title: url_friendly(product.product_name))}\' class=\'btn-cus quick-view btn_view ajax-link\' data-type=\'ajax\' rel=\'nofollow\' title=\'Xem nhanh sản phẩm\'><span>Xem nhanh</span></a>
+            <a href=\'#\' class=\'btn-cus quick-view btn_view btn_compare\' product_id=\'#{product.id}\' data-url=\'#{erp_online_store.add_to_compare_frontend_compare_items_path}\' rel=\'nofollow\' title=\'So sánh sản phẩm\'><span>So sánh</span></a>
         </div>".html_safe
       end
 
