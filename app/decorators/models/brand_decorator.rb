3a4c487fdec176a879d03f963537859e946b3060
Son Nguyen <sonnn@hoangkhang.com.vn>
2020-03-17 19:22:10 +0700

    update from server

--- app/decorators/models/brand_decorator.rb
+++ app/decorators/models/brand_decorator.rb
@@ -1,5 +1,5 @@
 Erp::Products::Brand.class_eval do
-  has_many :products, class_name: 'Erp::Products::BrandGroupDetail'
+  has_many :products, class_name: 'Erp::Products::Product'
   def self.get_stock_brands
     self.where(id: Erp::Products::Product.get_stock_inventory_products.distinct.pluck(:brand_id))
   end
