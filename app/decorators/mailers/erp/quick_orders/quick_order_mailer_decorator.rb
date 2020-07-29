93f19e89d6b0bd706ba4fc9f9f4575ffd92cf534
Son Nguyen <sonnn@hoangkhang.com.vn>
2020-07-29 09:28:50 +0700

    update

--- app/decorators/mailers/erp/quick_orders/quick_order_mailer_decorator.rb
+++ app/decorators/mailers/erp/quick_orders/quick_order_mailer_decorator.rb
@@ -1,18 +1,14 @@
 Erp::QuickOrders::QuickOrderMailer.class_eval do
-  
-  def sending_admin_email_order_confirmation(quick_order)
+  def sending_admin_email_order_confirmation(quick_order)    
     @recipients = ['Kinh Doanh <kinhdoanh@hoangkhang.com.vn>']
-    @bccmails = ['Hùng Nguyễn <hungnt@hoangkhang.com.vn>', 'Luân Phạm <luanpm@hoangkhang.com.vn>', 'Sơn Nguyễn <sonnn@hoangkhang.com.vn>', 'Nguyên Đỗ <nguyendtd@hoangkhang.com.vn>']
-    #@recipients = ['Sơn Nguyễn <sonnn@hoangkhang.com.vn>']
-    #@bccmails = []
+    @bccmails = ['Trương Thị Thanh Huyền <huyenttt@hoangkhang.com.vn>', 'Đỗ Thị Đa Nguyên <nguyendtd@hoangkhang.com.vn>']
     
     @quick_order = quick_order
-    send_email(@recipients.join("; "), @bccmails.join("; "), "[THCN] -#{Time.current.strftime('%Y%m%d')}- YÊU CẦU ĐẶT HÀNG TRÊN TÌM HÀNG CÔNG NGHỆ")
+    send_email(@recipients.join("; "), @bccmails.join("; "), "[Tìm Hàng Công Nghệ] - Đơn Đặt Đặt Hàng #{Time.current.strftime('%Y%m%d')}")
   end
   
   def sending_customer_email_order_confirmation(quick_order)
     @quick_order = quick_order
-    send_email(@quick_order.email, '', "#{Time.current.strftime('%Y%m%d')} - XÁC NHẬN ĐƠN ĐẶT HÀNG")
+    send_email(@quick_order.email, '', "[Tìm Hàng Công Nghệ] - Xác Nhận Đơn Hàng #{Time.current.strftime('%Y%m%d')}")
   end
-  
 end
